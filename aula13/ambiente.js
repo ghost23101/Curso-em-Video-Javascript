1bd0bc9c63e82a769cb6e6363b36386b3711c9ee
isaque-dev-web <126116466+isaque-dev-web@users.noreply.github.com>
2026-03-04 15:26:12 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 100)
+
+// var c = 1
+// while (c <= 5) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+// console.log('tudo bem?')
