63aadc32b45846c52cd7b950c82eb90c007247e1
isaque-dev-web <126116466+isaque-dev-web@users.noreply.github.com>
2026-03-11 16:28:00 -0300

    exercício feito

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+function tabuada() {
+    let numero = document.getElementById('txtnum')
+    let selecao = document.getElementById('seltab')
+    let res = document.getElementById('res')
+    
+    let n = Number(numero.value)
+
+    if (numero.value.length == 0) {
+        res.innerHTML = 'Digite um número para gerar a tabuada.'
+        selecao.innerHTML = ""
+    } else {
+        res.innerHTML = ""
+        selecao.innerHTML = ""
+
+        for (let i = 1; i <= 10; i++) {
+           let item = document.createElement('option')
+           
+           item.value = `tab${i}`
+           item.text = `${n} x ${i} = ${n * i}`
+
+           selecao.appendChild(item)
+        }
+    }
+}
