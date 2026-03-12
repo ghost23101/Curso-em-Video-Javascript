1e04685f81e769a39419b03fe7cdfbf3ddaa53a3
isaque-dev-web <126116466+isaque-dev-web@users.noreply.github.com>
2026-03-12 15:07:48 -0300

    update

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -8,19 +8,27 @@ function contar() {
     var f = Number(fim.value)
     var pas = Number(passo.value)
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || pas <= 0) {
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = '[ERRO] Verifique os dados e tente novamente!'
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
+
+        if (pas <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            pas = 1
+        }
 
         if (ini < f) {
+            // Contagem crescente
             for (var c = ini; c <= f; c += pas) {
-                res.innerHTML += `${c} 👉`
+                res.innerHTML += `${c} \u{1F449}`
             }
-        } else{
+        } else {
+            // Contagem regressiva
             for (var c = ini; c >= f; c -= pas) {
-                res.innerHTML += `${c} 👉`
+                res.innerHTML += `${c} \u{1F449}`
             }
         }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
